c013c3fe0adbc580279c4d270bbb9d5646a68865
yystju <yystju@hotmail.com>
2018-02-18 15:17:06 +0800

    Added something...

--- src/main/java/shi/quan/sshtest/MAIN.java
+++ src/main/java/shi/quan/sshtest/MAIN.java
@@ -25,23 +25,31 @@ public class MAIN {
     @Autowired
     SSHClient sshClient;
 
-//    @Bean
-//    CommandLineRunner server() {
-//        return (args) -> {
-//            sshService.start();
-//
-//            System.out.println("Press any key...");
-//            System.in.read();
-//
-//
-//            sshService.shutdown();
-//        };
-//    }
-
     @Bean
-    CommandLineRunner client() {
+    CommandLineRunner server() {
         return (args) -> {
-            sshClient.open();
+            if(args.length > 0 && "server".equals(args[0])) {
+
+                sshService.start();
+
+                System.out.println("Press any key...");
+                System.in.read();
+
+
+                sshService.shutdown();
+            } else if (args.length >= 5 && "client".equals(args[0])) {
+                sshClient.shell(args[1], args[2], args[3], Integer.parseInt(args[4]));
+            } else if (args.length >= 9 && "local".equals(args[0])) {
+                sshClient.l(args[1], args[2], args[3], Integer.parseInt(args[4]), args[5], Integer.parseInt(args[6]), args[7], Integer.parseInt(args[8]));
+            } else if (args.length >= 9 && "remote".equals(args[0])) {
+                sshClient.r(args[1], args[2], args[3], Integer.parseInt(args[4]), args[5], Integer.parseInt(args[6]), args[7], Integer.parseInt(args[8]));
+            } else {
+                System.out.println("Usage java -jar sshtest.jar [server|client|local|remote]");
+                System.out.println("\tserver -- No parameters.");
+                System.out.println("\tclient -- [username] [password] [hostname] [port]");
+                System.out.println("\tlocal -- [username] [password] [hostname] [port] [localhost] [localport] [remotehost] [remoteport]");
+                System.out.println("\tremote -- [username] [password] [hostname] [port] [remotehost] [remoteport] [localhost] [localport]");
+            }
         };
     }
 }
--- src/main/java/shi/quan/sshtest/SSHClient.java
+++ src/main/java/shi/quan/sshtest/SSHClient.java
@@ -8,7 +8,6 @@ import org.apache.sshd.client.keyverifier.AcceptAllServerKeyVerifier;
 import org.apache.sshd.client.session.ClientSession;
 import org.apache.sshd.client.session.forward.ExplicitPortForwardingTracker;
 import org.apache.sshd.common.channel.PtyMode;
-import org.apache.sshd.common.config.keys.FilePasswordProvider;
 import org.apache.sshd.common.forward.DefaultForwarderFactory;
 import org.apache.sshd.common.util.io.NoCloseInputStream;
 import org.apache.sshd.common.util.io.NoCloseOutputStream;
@@ -18,7 +17,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.Map;
 
@@ -27,7 +25,7 @@ public class SSHClient {
 
     private static Logger logger = LoggerFactory.getLogger(SSHClient.class);
 
-    public void open() {
+    public void shell(String userName, String password, String sshServerHost, int sshServerPort) {
         try {
             try(SshClient client = SshClient.setUpDefaultClient()) {
                 client.setServerKeyVerifier(AcceptAllServerKeyVerifier.INSTANCE);
@@ -36,68 +34,131 @@ public class SSHClient {
                 client.setForwardingFilter(new AcceptAllForwardingFilter());
                 client.setForwarderFactory(new DefaultForwarderFactory());
 
-                client.setFilePasswordProvider(new FilePasswordProvider() {
-                    @Override
-                    public String getPassword(String s) throws IOException {
-                        logger.info("s : {}", s);
-                        return "passw0rd";
-                    }
-                });
+//                client.setFilePasswordProvider(new FilePasswordProvider() {
+//                    @Override
+//                    public String getPassword(String s) throws IOException {
+//                        logger.info("s : {}", s);
+//                        return "passwd";
+//                    }
+//                });
 
-                ConnectFuture connectFuture = client.connect("root", "localhost", 2222);
+                ConnectFuture connectFuture = client.connect(userName, sshServerHost, sshServerPort);
 
                 connectFuture.await();
 
                 try(ClientSession session = connectFuture.getSession()) {
-                    session.addPasswordIdentity("passw0rd");
+                    session.addPasswordIdentity(password);
 
                     session.auth().verify(10000);
 
-                    SshdSocketAddress localAddress = new SshdSocketAddress("localhost", 4444);
-                    SshdSocketAddress remoteAddress = new SshdSocketAddress("localhost", 2222);
+                    try(ChannelShell channel = session.createShellChannel()) {
+                        logger.info("channel.getPtyType() : {}", channel.getPtyType());
+                        logger.info("channel.getPtyModes() : {}", channel.getPtyModes());
 
-                    try (ExplicitPortForwardingTracker localTracker = session.createLocalPortForwardingTracker(localAddress, remoteAddress)) {
-                        boolean isLocalForwarding = localTracker.isLocalForwarding();
+                        channel.setPtyType("xterm-256color");
+
+                        Map<PtyMode, Integer> modes = channel.getPtyModes();
+
+                        for(PtyMode key : modes.keySet()) {
+                            if(key == PtyMode.ECHO) {
+                                modes.put(key, 0);
+                            } else if(key == PtyMode.ECHOCTL) {
+                                modes.put(key, 0);
+                            } else if(key == PtyMode.ECHOE) {
+                                modes.put(key, 0);
+                            } else if(key == PtyMode.ECHOK) {
+                                modes.put(key, 0);
+                            } else if(key == PtyMode.ECHOKE) {
+                                modes.put(key, 0);
+                            } else if(key == PtyMode.ECHONL) {
+                                modes.put(key, 0);
+                            }
+                        }
+
+                        channel.setPtyModes(modes);
+
+                        channel.setIn(new NoCloseInputStream(System.in));
+                        channel.setOut(new NoCloseOutputStream(System.out));
+                        channel.setErr(new NoCloseOutputStream(System.err));
+
+                        channel.open();
+
+                        channel.waitFor(Arrays.asList(new ClientChannelEvent[]{ClientChannelEvent.CLOSED}), 0);
+                    } finally {
+                        session.close(false);
+                    }
+                } finally {
+                    client.stop();
+                }
+            }
+        } catch(Exception ex) {
+            logger.error(ex.getMessage(), ex);
+        }
+    }
+
+    public void l(String userName, String password, String sshServerHost, int sshServerPort, String localHost, int localPort, String remoteHost, int remotePort) {
+        try {
+            try(SshClient client = SshClient.setUpDefaultClient()) {
+                client.setServerKeyVerifier(AcceptAllServerKeyVerifier.INSTANCE);
+                client.start();
+
+                client.setForwardingFilter(new AcceptAllForwardingFilter());
+                client.setForwarderFactory(new DefaultForwarderFactory());
+
+                ConnectFuture connectFuture = client.connect(userName, sshServerHost, sshServerPort);
+
+                connectFuture.await();
+
+                try(ClientSession session = connectFuture.getSession()) {
+                    session.addPasswordIdentity(password);
+
+                    session.auth().verify(10000);
+
+                    SshdSocketAddress localAddress = new SshdSocketAddress(localHost, localPort);
+                    SshdSocketAddress remoteAddress = new SshdSocketAddress(remoteHost, remotePort);
+
+                    try (ExplicitPortForwardingTracker remoteTracker = session.createLocalPortForwardingTracker(localAddress, remoteAddress)) {
+                        System.out.println("Press any key...");
+                        System.in.read();
                     } finally {
                         session.close();
                     }
+                } finally {
+                    client.stop();
+                }
+            }
+        } catch(Exception ex) {
+            logger.error(ex.getMessage(), ex);
+        }
+    }
 
-//                    try(ChannelShell channel = session.createShellChannel()) {
-//                        logger.info("channel.getPtyType() : {}", channel.getPtyType());
-//                        logger.info("channel.getPtyModes() : {}", channel.getPtyModes());
-//
-//                        channel.setPtyType("xterm-256color");
-//
-//                        Map<PtyMode, Integer> modes = channel.getPtyModes();
-//
-//                        for(PtyMode key : modes.keySet()) {
-//                            if(key == PtyMode.ECHO) {
-//                                modes.put(key, 0);
-//                            } else if(key == PtyMode.ECHOCTL) {
-//                                modes.put(key, 0);
-//                            } else if(key == PtyMode.ECHOE) {
-//                                modes.put(key, 0);
-//                            } else if(key == PtyMode.ECHOK) {
-//                                modes.put(key, 0);
-//                            } else if(key == PtyMode.ECHOKE) {
-//                                modes.put(key, 0);
-//                            } else if(key == PtyMode.ECHONL) {
-//                                modes.put(key, 0);
-//                            }
-//                        }
-//
-//                        channel.setPtyModes(modes);
-//
-//                        channel.setIn(new NoCloseInputStream(System.in));
-//                        channel.setOut(new NoCloseOutputStream(System.out));
-//                        channel.setErr(new NoCloseOutputStream(System.err));
-//
-//                        channel.open();
-//
-//                        channel.waitFor(Arrays.asList(new ClientChannelEvent[]{ClientChannelEvent.CLOSED}), 0);
-//                    } finally {
-//                        session.close(false);
-//                    }
+    public void r(String userName, String password, String sshServerHost, int sshServerPort, String remoteHost, int remotePort, String localHost, int localPort) {
+        try {
+            try(SshClient client = SshClient.setUpDefaultClient()) {
+                client.setServerKeyVerifier(AcceptAllServerKeyVerifier.INSTANCE);
+                client.start();
+
+                client.setForwardingFilter(new AcceptAllForwardingFilter());
+                client.setForwarderFactory(new DefaultForwarderFactory());
+
+                ConnectFuture connectFuture = client.connect(userName, sshServerHost, sshServerPort);
+
+                connectFuture.await();
+
+                try(ClientSession session = connectFuture.getSession()) {
+                    session.addPasswordIdentity(password);
+
+                    session.auth().verify(10000);
+
+                    SshdSocketAddress localAddress = new SshdSocketAddress(localHost, localPort);
+                    SshdSocketAddress remoteAddress = new SshdSocketAddress(remoteHost, remotePort);
+
+                    try (ExplicitPortForwardingTracker localTracker = session.createRemotePortForwardingTracker(remoteAddress, localAddress)) {
+                        System.out.println("Press any key...");
+                        System.in.read();
+                    } finally {
+                        session.close();
+                    }
                 } finally {
                     client.stop();
                 }
